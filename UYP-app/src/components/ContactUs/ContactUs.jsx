6799c4b9dc00c2a2b9a324eafa8fcbce52dc23dc
Lypher <acosta.enzo@hotmail.com>
2023-06-27 09:04:44 -0300

    Componentes

---
+++ UYP-app/src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function ContactUs() {
+  return (
+    <div>ContactUs</div>
+  )
+}
+export default ContactUs;
---
+++ UYP-app/src/components/Error404/Error404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./Error404.module.css"
+
+ function Error404() {
+  return (
+    <div>Error404</div>
+  )
+}
+export default Error404
